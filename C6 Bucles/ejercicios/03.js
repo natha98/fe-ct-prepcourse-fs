873c105533e772e46dca1873aabee1c162f5c0f2
NAGACE <nathagc@gmail.com>
2024-07-25 12:48:33 -0500

    Update 03.js

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,16 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+
+    if(x > y ){
+      return x;
+    }
+     if(y > x){
+      return y;
+     }
+     if (x ==y){
+      return "cualquiera de los dos";
+     }
 }
 
 module.exports = obtenerMayor;
